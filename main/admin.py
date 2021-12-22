758c187f4bd690f62bd7dcf0d7a37e9a928ec9bb
andyundefined <5justandrew@gmail.com>
2021-12-22 21:00:36 +0200

    Configured admin and changed `DateTimeField` to `DateField` in Game model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
 # Register your models here.
+from django.contrib.admin import ModelAdmin
+
+from main.models import GameCategory, Game, Post
+
+
+@admin.register(GameCategory)
+class GameCategoryAdmin(ModelAdmin):
+    fields = ('name', 'slug')
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('id',)
+    list_editable = ('name', 'slug')
+    search_fields = ('name',)
+
+
+@admin.register(Game)
+class GameAdmin(ModelAdmin):
+    fields = ('category', 'slug', 'name', 'description', 'image', 'pub_date')
+    list_display = ('id', 'category', 'name', 'pub_date')
+    list_display_links = ('id',)
+    list_editable = ('name',)
+    list_filter = ('category',)
+    ordering = ('pub_date',)
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(ModelAdmin):
+    fields = ('game', 'title', 'content', 'pub_date', 'last_update', 'in_archive')
+    list_display = ('id', 'title', 'pub_date', 'last_update', 'in_archive')
+    list_editable = ('title', 'in_archive')
+    readonly_fields = ('pub_date', 'last_update')
+    list_display_links = ('id',)
+    search_fields = ('title',)
--- main/models.py
+++ main/models.py
@@ -21,16 +21,16 @@ class Game(models.Model):
     name = models.CharField(max_length=50, verbose_name='Game name')
     description = models.TextField(verbose_name='Game description')
     image = models.ImageField(default='None/no-image.png', upload_to='images/%Y/%m/%d', verbose_name='Game image')
-    pub_date = models.DateTimeField(verbose_name='Game publication date')
+    pub_date = models.DateField(verbose_name='Game publication date')
 
     def __str__(self):
         return self.name
 
 
 class Post(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.PROTECT, verbose_name='Game')
     title = models.CharField(max_length=100, verbose_name='Post title')
     content = models.TextField(verbose_name='Post content')
-    game = models.ForeignKey(Game, on_delete=models.PROTECT, verbose_name='Game')
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Post publication time')
     last_update = models.DateTimeField(auto_now=True, verbose_name='Post last updated time')
     in_archive = models.BooleanField(default=False, verbose_name='Post is in archive')
